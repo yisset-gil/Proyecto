10a4d7bc8fa955198efde3b467a3eb35075719d6
Yisset gil <yissetgil525@gmail.com>
2021-03-19 07:47:33 -0500

    commit 2: agregando archivo pagina2.php algo con contcatenacion

---
+++ pagina2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Proyecto Heroku</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" content="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+<body>
+	
+    <?php
+    $a =12;
+    $b = 1000;
+    
+    $a .= $b;
+
+    echo "<br /> contcatenando $a ."
+    ?>
+</body>
+</html>
