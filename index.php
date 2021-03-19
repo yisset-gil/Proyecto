f79f3f5bf3304383ce0b95ba271b8d726e0e29c7
Yisset Dayana Gil Gallego <ygil@todo1.com.co>
2021-03-18 20:18:59 -0500

    primer commit: Se creo el proyecto con un directorio css que tiene dos archivos css y un archivo index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Proyecto Heroku</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" content="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+<body>
+	<div id= "envoltura">
+	<header>
+	<h1>Software 2</h1>
+	</header>
+	<div id="main">
+	  <p>
+	      En este curso aprender&aacute; a programasr p&aacute;ginas web
+
+		  <?php
+		     $curso= "Construccion de elementos de software 2";
+			echo "<br /> <br />Todo lo necesario $curso ."
+		  ?>
+	  </p>
+
+	</div>
+	</div>
+</body>
+</html>
